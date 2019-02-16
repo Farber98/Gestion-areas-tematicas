9d931c3e5b0266615df7fc43fe18f75d8007a5b2
juan <juan@juan-535U3C>
2019-02-16 20:57:12 -0300

    Implementamos los metodos que faltaban para que el proyecto empiece a funcionar. Falta pedir algunos metodos a la clase trabajo para poder probar el programa. El programa deberia andar despues de conseguir estos metodos. IMPORTANTE: PRUEBA DE ERRORES Y AGREGAR DOCUMENTACION.

--- src/gui/areas/controladores/ControladorAreas.java
+++ src/gui/areas/controladores/ControladorAreas.java
@@ -5,14 +5,18 @@
  */
 package gui.areas.controladores;
 
+import gui.areas.modelos.Areas;
+import gui.areas.modelos.GestorAreas;
 import gui.areas.modelos.ModeloTablaAreas;
 import gui.areas.vistas.VentanaAreas;
 import gui.interfaces.IControladorAMArea;
 import gui.interfaces.IControladorAreas;
+import gui.interfaces.IGestorAreas;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 
 /**
@@ -22,6 +26,7 @@ import javax.swing.JTable;
 public class ControladorAreas implements IControladorAreas 
 {
     private VentanaAreas vista;
+    private int dameAreaSelec;
     
     
     public ControladorAreas(JFrame v) 
@@ -36,13 +41,30 @@ public class ControladorAreas implements IControladorAreas
     @Override
     public void btnBorrarClic(ActionEvent evt) 
     {
-        //Como implementar metodo
+        GestorAreas gestorAreas = GestorAreas.instanciar();
+        Areas unArea = this.dameAreaSeleccionada();
+        if(unArea == null)          //No hay ningun area seleccionada.
+            gestorAreas.cancelar();
+        else                        //Hay un area seleccionada.
+        {
+            int opcion = JOptionPane.showConfirmDialog(vista, "¿Desea borrar el area?","Borrar area", JOptionPane.YES_NO_OPTION);
+            if(opcion == JOptionPane.YES_OPTION)
+            {
+                String resultadoOperacion = gestorAreas.borrarArea(unArea);     //Devolvemos el resultado del metodo borrarArea.
+                if(!resultadoOperacion.equals(IGestorAreas.EXITO_BORRAR_AREA))  //Si el resultado no es exitoso, cancelamos la operacion.
+                {
+                    gestorAreas.cancelar();
+                    JOptionPane.showMessageDialog(vista, "No se pudo borrar el area", "Error al borrar area", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+            
+        }
     }
 
     @Override
     public void btnBuscarClic(ActionEvent evt)
     {
-        //Como implementar metodo
+        this.buscar();
     }
 
     @Override
@@ -75,15 +97,35 @@ public class ControladorAreas implements IControladorAreas
     private void buscar()
     {
         String nombreArea;
-        //Como implementar metodo
+        if(!this.vista.getTxtBuscar().getText().isEmpty())
+        {
+            nombreArea = this.vista.getTxtBuscar().getText();
+            ModeloTablaAreas modeloTabla = new ModeloTablaAreas(nombreArea);
+            JTable tabla = this.vista.getTablaAreas();
+            tabla.setModel(modeloTabla);
+        }
     }
     
     private void configurarTabla(JTable tablaAreas)
     {
-        ModeloTablaAreas modeloTabla = new ModeloTablaAreas(); //Que va en el parentesis?
+        ModeloTablaAreas modeloTabla = new ModeloTablaAreas(null); 
         tablaAreas.setModel(modeloTabla);
-        
-        //Chequear implementacion
+    }
+    
+    private Areas dameAreaSeleccionada()
+    {
+        JTable tabla = this.vista.getTablaAreas();
+        if(tabla.getSelectedRow() == -1)        //No hay ninguna fila seleccionada.
+        {
+            this.dameAreaSelec = -1;
+            return null;
+        }
+        else        //Hay alguna fila seleccionada.
+        {
+            ModeloTablaAreas modeloTabla = (ModeloTablaAreas) tabla.getModel();
+            this.dameAreaSelec = tabla.getSelectedRow();
+            return modeloTabla.obtenerArea(tabla.getSelectedRow());
+        }
     }
     
     
